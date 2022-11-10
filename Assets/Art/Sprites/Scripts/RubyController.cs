6d9582eb175e521610c4edf01737f8b63878cc6b
Gravityguy15 <113185554+Gravityguy15@users.noreply.github.com>
2022-11-10 08:40:55 -0500

    Part 6 complete

--- Assets/Art/Sprites/Scripts/RubyController.cs
+++ Assets/Art/Sprites/Scripts/RubyController.cs
@@ -1,26 +1,36 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 60;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Player Movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+    }
+    private void FixedUpdate() 
+    {
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
-        transform.position = position;
-    } 
+        position.x = position.x + 5f * horizontal * Time.deltaTime;
+        position.y = position.y + 5f * vertical * Time.deltaTime;
+        
+        rigidbody2d.MovePosition(position);
+
+    }
 }
+
+
