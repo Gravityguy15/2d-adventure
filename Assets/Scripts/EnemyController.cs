1339e96550d1c9f783bb981dd526ceed07c58846
Gravityguy15 <113185554+Gravityguy15@users.noreply.github.com>
2023-01-20 09:09:20 -0500

    Part 12 complete

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,6 +16,8 @@ public class EnemyController : MonoBehaviour
 
     bool broken = true;
 
+    public ParticleSystem smokeEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -80,5 +82,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
